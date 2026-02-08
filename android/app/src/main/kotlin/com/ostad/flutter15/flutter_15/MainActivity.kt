47de73f19b9f739084574e755d1fc9fd793fca9c
taufiqurSabbir <taufiqur2511@gmail.com>
2026-02-08 23:27:50 +0600

    class-2

---
+++ android/app/src/main/kotlin/com/ostad/flutter15/flutter_15/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ostad.flutter15.flutter_15
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home.dart
@@ -0,0 +1,383 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter - 15'),
+          centerTitle: true,
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+          
+            children: [
+              Text(
+                'Hello flutter',
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w100),
+              ),
+              Text(
+                'Hello flutter',
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w100),
+              ),
+              Text(
+                'Hello flutter',
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w100),
+              ),
+              Text(
+                'Hello flutter',
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w100),
+              ),
+              Text(
+                'Hello flutter',
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w100),
+              ),
+              Text(
+                'Hello flutter',
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w100),
+              ),
+              Text(
+                'Hello flutter',
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w100),
+              ),
+              Text(
+                'Hello flutter',
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w100),
+              ),
+            ],
+          ),
+        )
+
+        // SingleChildScrollView(
+        //   child: Column(
+        //     children: [
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //       Text(
+        //         'Hello flutter',
+        //         style: TextStyle(
+        //             fontSize: 25,
+        //             color: Colors.blue,
+        //             fontWeight: FontWeight.w100),
+        //       ),
+        //     ],
+        //   ),
+        // )
+
+        );
+  }
+}
